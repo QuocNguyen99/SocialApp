0cc3eb8b69f8808665f4218b51d2866427c3ddd6
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2020-11-14 04:04:29 +0700

    edit folder

--- App/components/Post/ItemPost.js
+++ App/components/Post/ItemPost.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
-import Avata from '../Avata'
+import Avata from './Avata'
 
 export default function ItemPost({ url, name, time, content }) {
     const [height, setHeight] = useState();
@@ -14,28 +13,26 @@ export default function ItemPost({ url, name, time, content }) {
         )
     }, [])
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Avata />
-                    <View style={styles.subContainer}>
-                        <Text style={styles.name}>Huỳnh Quốc Nguyên</Text>
-                        <Text style={styles.time}>7 giờ trước</Text>
-                    </View>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Avata />
+                <View style={styles.subContainer}>
+                    <Text style={styles.name}>Huỳnh Quốc Nguyên</Text>
+                    <Text style={styles.time}>7 giờ trước</Text>
                 </View>
-                <View style={styles.contentContainer}>
-                    <Text style={styles.content}>Today I feel so good</Text>
-                </View>
-                <Image
-                    resizeMode='stretch'
-                    style={{
-                        height: height,
-                    }}
-                    source={{
-                        uri: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t1.0-9/36762009_489593304810529_4217376718932934656_n.jpg?_nc_cat=104&ccb=2&_nc_sid=09cbfe&_nc_ohc=HlpGtuYL9ooAX9ML2xe&_nc_ht=scontent.fsgn5-5.fna&oh=60e7c6876f08ade9fbf2c68ddde15c04&oe=5FD386D1',
-                    }} />
             </View>
-        </ScrollView>
+            <View style={styles.contentContainer}>
+                <Text style={styles.content}>Today I feel so good</Text>
+            </View>
+            <Image
+                resizeMode='stretch'
+                style={{
+                    height: height,
+                }}
+                source={{
+                    uri: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t1.0-9/36762009_489593304810529_4217376718932934656_n.jpg?_nc_cat=104&ccb=2&_nc_sid=09cbfe&_nc_ohc=HlpGtuYL9ooAX9ML2xe&_nc_ht=scontent.fsgn5-5.fna&oh=60e7c6876f08ade9fbf2c68ddde15c04&oe=5FD386D1',
+                }} />
+        </View>
     )
 }
 
--- App/components/Post/Post.js
+++ App/components/Post/Post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import ItemPost from './ItemPost/ItemPost'
+import ItemPost from './ItemPost'
 
 
 export default function Post() {
