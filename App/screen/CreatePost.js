283617a3cba55f09366db951d3c3d2d8b5464f00
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2020-11-15 20:41:47 +0700

    config styles createpost

--- App/screen/CreatePost.js
+++ App/screen/CreatePost.js
@@ -6,10 +6,10 @@ import Title from '../components/Title';
 
 export default function CreatePost({ closeModal }) {
     return (
-        <View style={{ flex: 1 }} >
+        <View style={styles.container} >
             <View style={styles.headerContainer}>
                 <TouchableHighlight
-                    style={{ borderRadius: 30, padding: 10 }}
+                    style={styles.icon}
                     underlayColor='gray'
                     onPress={closeModal}>
                     <Icon name='arrow-left' color='black' size={20} />
@@ -19,21 +19,24 @@ export default function CreatePost({ closeModal }) {
             <View style={styles.containerContent}>
                 <ScrollView>
                     <KeyboardAvoidingView behavior='padding'>
-                        <TextInput placeholder='What are you thinking?' multiline={true} />
+                        <TextInput placeholder='What are you thinking?' multiline={true} style={styles.input} />
                     </KeyboardAvoidingView>
                 </ScrollView>
             </View>
-            <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                <View style={{ flex: 0.5, position: 'absolute', backgroundColor: 'yellow', height: 200, width: '100%', flexDirection: 'row' }}>
+            <View style={styles.footerContainer}>
+                <View style={styles.subFooter}>
                     <View style={{ backgroundColor: 'blue', height: 50, width: 50 }}></View>
                     <View style={{ backgroundColor: 'red', height: 50, width: 50 }}></View>
                 </View>
             </View>
-        </View >
+        </ View >
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
     containerContent: {
         flex: 1,
         paddingHorizontal: 10
@@ -45,5 +48,23 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         borderBottomColor: 'lightgray',
         borderWidth: 1
+    },
+    footerContainer: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    icon: {
+        borderRadius: 30,
+        padding: 10
+    },
+    input: {
+        fontSize: 16
+    },
+    subFooter: {
+        flex: 0.5,
+        position: 'absolute',
+        backgroundColor: 'yellow',
+        height: 200, width: '100%',
+        flexDirection: 'row'
     }
 })
